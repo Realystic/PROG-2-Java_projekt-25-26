d598befe3217854062e42f24b5fb6cca81e57b85
Realystic <stefan.ilija1@gamail.com>
2026-05-29 10:57:09 +0200

    feat: datoteka za glavni meni

---
+++ Glavni_menu.java
@@ -0,0 +1,37 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Glavni_menu extends JFrame	{
+	// Glavni meni (Main Menu) igre
+	/* Potrebuje se frame, panela, (action listener (ali mouse listener če nočemo gumba))
+	 * Naslov
+	 * klik za start
+	 * gumb	za izhod
+	 * (Minecraft main menu animacijo) (naslov se povečava in manjša z majhno naključno rotacijo (v obe smeri))
+	 */
+	private final String NASLOV = "Java Igra";
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public Glavni_menu() {
+		
+	}
+	
+	public void Show() {
+		
+	}
+}
